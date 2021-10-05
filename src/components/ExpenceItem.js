1c92d65309b950d9f5783af50d1744c94e77429c
EminGm <qasimovemin@gmail.com>
2021-10-05 14:49:41 +0400

    feat:Add dynamic data in Expence

--- src/components/ExpenceItem.js
+++ src/components/ExpenceItem.js
@@ -1,5 +1,18 @@
+import "./ExpenceItem.css";
+
 function ExpenceItem() {
-  return <h2>Expence Item</h2>;
+  const expenceItemDate = new Date(2001, 1, 23);
+  const expenceItemDescription = "Car Insurance";
+  const expenceItemPrice = "435";
+  return (
+    <div className="expense-item">
+      <div>{expenceItemDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenceItemDescription}</h2>
+        <div className="expense-item__price">${expenceItemPrice}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenceItem;
